1bfd0251d57e6428fab6a7aa9af5f27cdd319d15
smaruyam <sho.maruyama@cern.ch>
2015-10-13 14:06:54 -0500

    for runD with new miniaod formats

---
+++ TriggerDataAna/TriggerDataAna/test/runRunD.py
@@ -0,0 +1,78 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Test")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("Configuration.Geometry.GeometryIdeal_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.GlobalTag.globaltag = '74X_dataRun2_Prompt_v2'
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+inputFilesAOD = cms.untracked.vstring(
+    '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/AODSIM/PU20bx25_PHYS14_25_V1-v1/00000/00CC714A-F86B-E411-B99A-0025904B5FB8.root',
+    '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/AODSIM/PU20bx25_PHYS14_25_V1-v1/00000/040D9AF7-FB6B-E411-8106-0025907DBA06.root',
+    '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/AODSIM/PU20bx25_PHYS14_25_V1-v1/00000/04442001-036C-E411-9C90-0025901D42C0.root',
+    )    
+
+inputFilesMiniAOD = cms.untracked.vstring(
+#    '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/0432E62A-7A6C-E411-87BB-002590DB92A8.root',
+#    '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/06C61714-7E6C-E411-9205-002590DB92A8.root',
+#    '/store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/0EAD09A8-7C6C-E411-B903-0025901D493E.root'        ,
+#	'/store/data/Run2015B/SingleMuon/MINIAOD/PromptReco-v1/000/251/164/00000/544E58CD-A226-E511-834E-02163E0134D6.root'
+#	'/store/data/Run2015C/SingleMuon/MINIAOD/PromptReco-v1/000/254/833/00000/EAEF3DB9-104B-E511-9C21-02163E0139BA.root'
+	'/store/data/Run2015D/SingleMuon/MINIAOD/05Oct2015-v1/10000/021FD3F0-876F-E511-99D2-0025905A6060.root'
+    )
+
+useAOD = False
+
+if useAOD == True :
+    inputFiles = inputFilesAOD
+    outputFile = "myntuple.root"
+    print("AOD input files are used")
+else :
+    inputFiles = inputFilesMiniAOD
+    outputFile = "myntuple_mini.root"
+    print("MiniAOD input files are used")
+process.source = cms.Source ("PoolSource", fileNames = inputFiles )                             
+
+# Set up electron ID (VID framework)
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+if useAOD == True :
+    dataFormat = DataFormat.AOD
+else :
+    dataFormat = DataFormat.MiniAOD
+
+switchOnVIDElectronIdProducer(process, dataFormat)
+
+my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_25ns_V1_cff']
+
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+
+process.ntupler = cms.EDAnalyzer('TriggerAna',
+				triggerResults = cms.InputTag( 'TriggerResults', '', 'HLT' ),
+    prescales = cms.InputTag("patTrigger"),
+    objects = cms.InputTag("selectedPatTrigger"),
+				triggerName = cms.untracked.string("HLT_Mu10_TrkIsoVVL_DiPFJet40_DEta3p5_MJJ750_HTT350_PFMETNoMu60_JetIdCleaned_v"),
+				#triggerName = cms.untracked.string("HLT_Mu10_TrkIsoVVL_DiPFJet40_DEta3p5_MJJ750_HTT350_PFMETNoMu60_v"),
+                                 beamSpot = cms.InputTag('offlineBeamSpot'),
+                                 electrons    = cms.InputTag("gedGsfElectrons"),
+                                 muons    = cms.InputTag("muons"),
+                                 genParticles = cms.InputTag("genParticles"),
+                                 vertices     = cms.InputTag("offlinePrimaryVertices"),
+                                 conversions  = cms.InputTag('allConversions'),
+                                 electronsMiniAOD    = cms.InputTag("slimmedElectrons"),
+                                 muonsMiniAOD    = cms.InputTag("slimmedMuons"),
+                                 jetsMiniAOD    = cms.InputTag("slimmedJets"),
+                                 metsMiniAOD    = cms.InputTag("slimmedMETs"),
+                                 genParticlesMiniAOD = cms.InputTag("prunedGenParticles"),
+                                 verticesMiniAOD     = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                                 conversionsMiniAOD  = cms.InputTag('reducedEgamma:reducedConversions'),
+				eleLooseIdMap = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-loose")
+                                )
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string( outputFile )
+                                   )
+process.p = cms.Path(process.egmGsfElectronIDSequence * process.ntupler)
