00922d7bb682ccdc846dd394aaae25ba04435101
smaruyam <sho.maruyama@cern.ch>
2015-10-30 16:30:47 -0500

    debug for root 6

--- TriggerDataAna/TriggerDataAna/test/drawEff.cc
+++ TriggerDataAna/TriggerDataAna/test/drawEff.cc
@@ -12,9 +12,9 @@
 
 using namespace std;
 
-void drive(string sden, string dencut, string numcut, string caption, string fname, string leg, string rname);
+void drive(string sden, string dencut, string numcut, string caption, string fname, string leg, string rname, int ibins, float xmin, float xmax, bool isLog);
 
-void  CMS_lumi( TPad* pad, int iPeriod, int iPosX );
+void  CMS_lumi( TPad* pad, int iPeriod, int iPosX , string);
 
 void drawEff(){
 //string rname = "newRunDOct05.root"; // file name
@@ -203,7 +203,7 @@ float relExtraDY = 1.2;
 float extraOverCmsTextSize  = 0.76;
 
 TString lumi_13TeV = lumi + " fb^{-1}";
-TString lumi_8TeV  = "19.7 fb^{-1}";
+//TString lumi_8TeV  = "19.7 fb^{-1}";
 TString lumi_8TeV  = "18.5 fb^{-1}";
 TString lumi_7TeV  = "5.1 fb^{-1}";
 
